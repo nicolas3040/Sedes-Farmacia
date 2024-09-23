e74039b5fa68af4ed708d4f66ae5a9f330189320
ValeriaAgreda <acv5001715@est.univalle.edu>
2024-09-23 07:57:47 -0400

    Resueltos los conflictos en database.js y server.js

--- Backend/database.js
+++ Backend/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1860',
-  database: 'farmacias'
+  password: '1234',
+  database: 'sedes'
 });
 
 db.connect((err) => {
